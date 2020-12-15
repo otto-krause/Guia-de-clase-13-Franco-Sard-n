76d6d119840b5865ec746248e3a520861c9b15e4
gusgatam <gustavo.gatamora@gmail.com>
2020-12-15 19:19:40 -0300

    Devolución.
    Solo dos archivos tiene formato valido.

--- 1.cpp
+++ 1.cpp
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-int a,rtt=0,rt[]{1,2,3,4,5};
-for (a=0;a<5;a++){
+int main()//Sin funciones.
+{
+int a,rtt=0,rt[5]{0};//Falta tamaño
+for (a=0;a<5;a++)
+{
 printf("Ingrese las resistencias ");
-scanf("%d",&rt[a]);}
-for (a=0;a<5;a++){
+scanf("%d",&rt[a]);
+}
+for (a=0;a<5;a++)
+{
 printf("\n Las resistencias son %d",rt[a]);
 rtt=rtt+rt[a];
 }
